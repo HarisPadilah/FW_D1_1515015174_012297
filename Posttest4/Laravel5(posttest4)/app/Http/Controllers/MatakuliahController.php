ed82c7ce9e216f474431c8055131327e2f67c0d4
Febry D.N.R.P <Febry D.N.R.P>
2017-04-06 10:21:08 +0800

    Posttest4

---
+++ Posttest4/Laravel5(posttest4)/app/Http/Controllers/MatakuliahController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Matakuliah;
+
+class MatakuliahController extends Controller
+{
+    public function awal()
+    {
+        return view('matakuliah.awal', ['data'=> matakuliah::all()]);
+    }
+
+    public function tambah()
+    {
+        return view('matakuliah.tambah');
+    }
+
+    public function simpan(Requests $input)
+    {
+        $matakuliah =new matakuliah();
+        $matakuliah->title = $input->title;
+        $matakuliah->keterangan = $input->keterangan;
+        $informasi = $matakuliah->save() ? 'Berhasil simpan data' : 'Gagal simpan data';
+        return redirect('matakuliah') -> with(['informasi'=>$informasi]);
+    }
+    
+    public function edit($id)
+    {
+        $matakuliah = matakuliah::find($id);
+        return view('matakuliah.edit')-> with (array('matakuliah'=>$matakuliah));
+    }
+
+    public function lihat($id)
+    {
+        $matakuliah = matakuliah::find($id);
+        return view('matakuliah.lihat')-> with (array('matakuliah'=>$matakuliah));
+    }
+
+    public function update($id, Request $input)
+    {
+        $matakuliah = matakuliah::find($id);
+        $matakuliah->title = $input->title;
+        $matakuliah->keterangan = $input->keterangan;
+        $informasi = $matakuliah->save() ? 'Berhasil update data' : 'Gagal update data';
+        return redirect('matakuliah') -> with(['informasi'=>$informasi]);
+    }
+
+    public function hapus($id)
+    {
+        $matakuliah = matakuliah::find($id);
+        $informasi = $matakuliah->delete() ? 'Berhasil hapus data' : 'Gagal  data';
+        return redirect('matakuliah') -> with(['informasi'=>$informasi]);
+    }
+}
